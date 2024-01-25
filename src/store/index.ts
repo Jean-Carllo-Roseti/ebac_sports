64e9225168b49a86218d60e21d7ee0aa7d87a8c6
jean carllo <jeanrosete04@gmail.com>
2024-01-25 17:32:18 -0300

    efeutado o uso do carrinhoReduce e a propriedade adicionar.

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import carrinhoReducer from './reduce/carrinho'
 import favoritoReducer from './reduce/favorito'
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     carrinho: carrinhoReducer,
     favorita: favoritoReducer
   }
 })
+
+export type RootReducer = ReturnType<typeof store.getState>
